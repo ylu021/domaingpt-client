d7aeeef1fc5cafe381af1342f9fb9f05ff5021a8
ylu021 <yichen.luu@gmail.com>
2023-08-21 22:02:44 -0400

    change padding

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import { Logo, Menu, ProfileDisplay } from './components';
 
 const Sidebar = () => {
     return (
-        <Box minH="100vh" minW="200px" width="30%" bgColor={'blue.800'} paddingX="4" paddingY="6">
+        <Box minH="100vh" minW="200px" width="30%" bgColor={'blue.800'} paddingX="5" paddingY="6">
             <Flex height="100%" flexDir="column" justifyContent="space-between" alignItems="center">
                 <Logo />
                 <Menu />
